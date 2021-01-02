f2cf830fd72bfd668c9a9c48b4f15a07b6e51918
Nygel M <nygelmeece8@gmail.com>
2021-01-01 20:12:54 -0500

    PS3-4.1 deal_hand Wildcard and test update
    Beefed up unit test for the deal_hand function

--- MIT_OCW/MIT_OCW_PSets/PS3_Srabble/PS3/test_deal_hand.py
+++ MIT_OCW/MIT_OCW_PSets/PS3_Srabble/PS3/test_deal_hand.py
@@ -14,15 +14,30 @@ import ps3 as scrabble
 if type(scrabble.deal_hand(7)) == type(dict):
     print('True')
 
-n = 7 
-x = [7]
-y = {7:0}
-z = (7)
+# n = 7 
+#x = [7]
+#y = {7:0}
+#z = (7)
 
-print('When deal_hand is given', n, 'an object of type', str(type(n)), 'deal_hand returns:', scrabble.deal_hand(n))
+#print('When deal_hand is given', n, 'an object of type', str(type(n)), 'deal_hand returns:', scrabble.deal_hand(n))
 
-print('When deal_hand is given', x, 'an object of type', str(type(x)), 'deal_hand returns:', scrabble.deal_hand(x))
+#print('When deal_hand is given', x, 'an object of type', str(type(x)), 'deal_hand returns:', scrabble.deal_hand(x))
 
-print('When deal_hand is given', y, 'an object of type', str(type(y)), 'deal_hand returns:', scrabble.deal_hand(y))
+#print('When deal_hand is given', y, 'an object of type', str(type(y)), 'deal_hand returns:', scrabble.deal_hand(y))
 
-print('When deal_hand is given', z, 'an object of type', str(type(z)), 'deal_hand returns:', scrabble.deal_hand(z))
+#print('When deal_hand is given', z, 'an object of type', str(type(z)), 'deal_hand returns:', scrabble.deal_hand(z))
+
+def test_wildcard_deal_hand():
+    '''
+    Unit test for deal_hand
+    '''
+    # Test 1 - deal_hand given an int
+    n = 7
+    hand = scrabble.deal_hand(n)
+    if hand.get('*') == 1 and len(hand) == n:
+        print('SUCCESS: When given', type(n), 'of value:', n, '.')
+        print('deal_hand returns:', type(hand), 'of value:', str(hand))
+
+        return # exit function
+
+test_wildcard_deal_hand()
