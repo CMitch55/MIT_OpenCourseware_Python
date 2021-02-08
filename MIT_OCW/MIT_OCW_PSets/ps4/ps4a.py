4c981d16c1d52f000d9f46a5981f208ab9126c9f
Nygel M <nygelmeece8@gmail.com>
2021-02-08 13:47:31 -0500

    PS4a Recursive Permutation
    ps4a.py completed.

--- MIT_OCW/MIT_OCW_PSets/ps4/ps4a.py
+++ MIT_OCW/MIT_OCW_PSets/ps4/ps4a.py
@@ -22,8 +22,31 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     '''
+    try:
+        if len(sequence) <= 1:
+            return [sequence]
+    except TypeError:
+        return (print('Object of non-string type passed to get_permutations.'))
+    except NameError:
+        return (print('get_permutation argument undefined'))
+    except SyntaxError:
+        return (print('ger_permutation argument syntactically incorrect. Please use a string.'))
+    else:
+        permutations   = []
+        first_char     = sequence[0]
+        remaining_char = sequence[1:]
+        perms_of_subsequence = get_permutations(remaining_char)
+        
+        for seq in perms_of_subsequence:
+        
+            for n in range(len(seq) + 1):
+                new_seq = seq[0:n] + first_char + seq[n:len(seq) + 1]
+                permutations.append(new_seq)
 
-    pass #delete this line and replace with your code here
+        return permutations
+
+
+#print(get_permutations('abc'))
 
 if __name__ == '__main__':
 #    #EXAMPLE
@@ -32,9 +55,27 @@ if __name__ == '__main__':
 #    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
 #    print('Actual Output:', get_permutations(example_input))
     
-#    # Put three example test cases here (for your sanity, limit your inputs
-#    to be three characters or fewer as you will have n! permutations for a 
-#    sequence of length n)
+    print('Test 1')
+    print("Input:", "'pog'")
+    print('Expected Output:', ['pog', 'opg', 'ogp', 'gpo', 'gop', 'pgo'])
+    print('Actual Output:', get_permutations('pog'))
 
-    pass #delete this line and replace with your code here
+    print()
+    print('------------')
+    print()
+
+    print('Test 2')
+    print('Input:', "'po'")
+    print('Expected Output:', ['po', 'op'])
+    print('Actual Output:', get_permutations('po'))
+
+    print()
+    print('------------')
+    print()
+
+    print('Test 3')
+    print('Input:', "'p'")
+    print('Expected Output:', ['p'])
+    print('Actual Output:', get_permutations('p'))
+    
 
